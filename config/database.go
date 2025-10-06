725aeb5211c13b6dcf84514085c4bf94036688c3
AnamSadat <anamsadat3@gmail.com>
2025-10-06 23:50:31 +0700

    feat: add database

---
+++ config/database.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	dsn := "host=localhost user=postgres password=lumiere2327 dbname=movie_review port=5432 sslmode=disable"
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect database: ", err)
+	}
+
+	DB = database
+	fmt.Println("✅ Database Connect")
+}
