56988be5d5aba48bdc30a742abb97072e6a4f9b3
AnamSadat <anamsadat3@gmail.com>
2025-10-07 10:23:43 +0700

    feat(postgres): update database

--- config/database.go
+++ config/database.go
@@ -3,7 +3,9 @@ package config
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -11,13 +13,32 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := "host=localhost user=postgres password=lumiere2327 dbname=movie_review port=5432 sslmode=disable"
-	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
+	// Load file .env
+	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Failed to connect database: ", err)
+		log.Fatal("❌ Error loading .env file")
+	}
+
+	// Ambil nilai variabel dari environment
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+	sslmode := os.Getenv("SSL_MODE")
+
+	// Buat DSN string
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		host, user, password, dbname, port, sslmode,
+	)
+
+	// Koneksi ke DB
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("❌ Failed to connect to database:", err)
 	}
 
 	DB = database
-	fmt.Println("✅ Database Connect")
+	fmt.Println("✅ Database connected")
 }
